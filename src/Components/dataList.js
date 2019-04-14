b7448857d7023b5a9bdd8fd4ff442522e889be61
Abhishek Gangwar <abhishek.gangwar@MAC.local>
2019-04-14 14:11:24 +0530

    added code to show the difference with hooks usages

--- src/Components/dataList.js
+++ src/Components/dataList.js
@@ -4,6 +4,21 @@ import { fetchResourceHook } from "../hooks/fetchResourceHook";
 const DataList = props => {
   const resources = fetchResourceHook(props.selectedResource);
 
+  /***
+   * We Don't  need this code because Hooks seperated the logic out of component .. wohoooo
+   */
+
+  // const [resources, setResources] = useState([]);
+
+  // const fetchData = async () => {
+  //   const response = await fetchResources(props.selectedResource);
+  //   setResources(response.data);
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, [props.selectedResource]);
+
   return (
     <div className="ui container" style={{ textAlign: "left" }}>
       <ul>
